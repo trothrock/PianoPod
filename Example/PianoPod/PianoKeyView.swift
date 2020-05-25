6a73f9d712c7ad219499c5ca7db35e8baaf6e4f6
Ted Rothrock <theodore.rothrock@gmail.com>
2020-05-25 18:27:21 -0500

    Updated to accommodate landscape

---
+++ Example/PianoPod/PianoKeyView.swift
@@ -0,0 +1,88 @@
+//
+//  PianoKeyView.swift
+//  PianoPod_Example
+//
+//  Created by Theodore Rothrock on 5/25/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+extension CGFloat {
+    static let pianoKeyAspectRatio: CGFloat = 0.14
+}
+
+struct PianoKeyView: View {
+    
+    var note: Note
+    var color: Color
+    var naturalKeyWidth: CGFloat
+        
+    private var naturalKeySize: CGSize {
+        return CGSize(width: naturalKeyWidth, height: naturalKeyWidth / CGFloat.pianoKeyAspectRatio)
+    }
+    
+    private var accidentalKeySize: CGSize {
+        let width = naturalKeyWidth * 0.6
+        return CGSize(width: width, height: width / CGFloat.pianoKeyAspectRatio)
+    }
+    
+    private var radius: CGFloat {
+        let divisor: CGFloat = isAccidental ? 9.0 : 7.0
+        return naturalKeyWidth / divisor
+    }
+    
+    private var isAccidental: Bool {
+        let accidentals: [Pitch] = [.cSharp, .dSharp, .fSharp, .gSharp, .aSharp]
+        return accidentals.contains(note.pitch)
+    }
+    
+    private struct PianoKeyButtonStyle: ButtonStyle {
+        var isAccidental: Bool
+        
+        private var verticalTranslation: CGFloat {
+            return isAccidental ? 1.0 : 1.005
+        }
+        
+        private var horizontalTranslation: CGFloat {
+            return isAccidental ? 0.9 : 0.94
+        }
+        
+        func makeBody(configuration: Self.Configuration) -> some View {
+            configuration.label
+                .scaleEffect(x: configuration.isPressed ? horizontalTranslation : 1.0, y: configuration.isPressed ? verticalTranslation : 1.0, anchor: .top)
+        }
+    }
+    
+    private struct PianoKeyShape: Shape {
+        var isAccidental: Bool
+        var radius: CGFloat
+        
+        func path(in rect: CGRect) -> Path {
+            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: radius, height: radius))
+            return Path(path.cgPath)
+        }
+    }
+    
+    var body: some View {
+        Button(action: {
+            print("Play \(self.note.pitch.rawValue) \(self.note.octave)")
+        }, label: {
+            PianoKeyShape(isAccidental: isAccidental, radius: radius)
+                .foregroundColor(self.color)
+                .overlay(
+                    PianoKeyShape(isAccidental: isAccidental, radius: radius)
+                        .stroke(Color.black, lineWidth: 0.5)
+                )
+        })
+        .frame(width: isAccidental ? accidentalKeySize.width : naturalKeySize.width, height: isAccidental ? accidentalKeySize.height : naturalKeySize.height)
+        .buttonStyle(PianoKeyButtonStyle(isAccidental: isAccidental))
+    }
+
+}
+
+struct PianoKeyView_Previews: PreviewProvider {
+    static var previews: some View {
+        PianoKeyView(note: Note(pitch: .c, octave: 0), color: .white, naturalKeyWidth: 100)
+    }
+}
--- Example/PianoPod/PianoView.swift
+++ Example/PianoPod/PianoView.swift
@@ -18,6 +18,13 @@ struct PianoView: View {
     @State private var bottomOctave: Int = 0
     @State private var visibleOctaves: Int = 1
     
+    private let toolbarControlHeight: CGFloat  = 35
+    private let toolbarPadding: CGFloat  = 7
+    
+    private var toolbarHeight: CGFloat {
+        return toolbarControlHeight + (toolbarPadding * 2)
+    }
+    
     private var octaves: [Octave] {
         var result = [Octave]()
         for octaveIndex in bottomOctave..<bottomOctave + visibleOctaves {
@@ -37,12 +44,14 @@ struct PianoView: View {
                         Image(systemName: "gear")
                             .resizable()
                     })
-                    .frame(width: 35, height: 35)
+                    .frame(width: self.toolbarControlHeight, height: self.toolbarControlHeight)
                     .foregroundColor(self.accentColor)
-                    .padding([.top, .bottom, .trailing], 10)
+                    .padding(self.toolbarPadding)
                 }
+                .frame(height: self.toolbarHeight)
                 
                 Spacer()
+                    .frame(minHeight: 0)
                 
                 ZStack(alignment: .top) {
                     HStack(spacing: 0) {
@@ -73,10 +82,11 @@ struct PianoView: View {
                                     .frame(width: self.accidentalEndSpacing(for: geometry.size), height: 0)
                             }
                         }
-                    }
+                    }.frame(width: (self.naturalKeyWidth(for: geometry.size) * 7) * CGFloat(self.visibleOctaves))
                 }
 
                 Spacer()
+                    .frame(minHeight: 0)
             }
         }
         .sheet(isPresented: $isPresentingSettings, content: { SettingsView(isPresented: self.$isPresentingSettings, bottomOctave: self.$bottomOctave, visibleOctaves: self.$visibleOctaves) })
@@ -84,7 +94,13 @@ struct PianoView: View {
     }
     
     private func naturalKeyWidth(for size: CGSize) -> CGFloat {
-        return size.width / (7 * CGFloat(visibleOctaves))
+        let availableHeight = size.height - toolbarHeight
+        let fullScreenWidth = size.width / (7 * CGFloat(visibleOctaves))
+        let fullScreenHeight = fullScreenWidth / CGFloat.pianoKeyAspectRatio
+        if fullScreenHeight > availableHeight {
+            return availableHeight * CGFloat.pianoKeyAspectRatio
+        }
+        return fullScreenWidth
     }
     
     private func accidentalInterKeySpacing(for size: CGSize) -> CGFloat {
@@ -97,91 +113,12 @@ struct PianoView: View {
     
 }
 
-struct PianoKeyView: View {
-    
-    var note: Note
-    var color: Color
-    var naturalKeyWidth: CGFloat
-        
-    private var naturalKeySize: CGSize {
-        let naturalKeyAspectRatio: CGFloat = 2.2/15.0
-        return CGSize(width: naturalKeyWidth, height: naturalKeyWidth / naturalKeyAspectRatio)
-    }
-    
-    private var accidentalKeySize: CGSize {
-        let accidentalKeyAspectRatio: CGFloat = 0.14
-        let width = naturalKeyWidth * 0.6
-        return CGSize(width: width, height: width / accidentalKeyAspectRatio)
-    }
-    
-    private var radius: CGFloat {
-        let divisor: CGFloat = isAccidental ? 9.0 : 7.0
-        return naturalKeyWidth / divisor
-    }
-    
-    private var isAccidental: Bool {
-        let accidentals: [Pitch] = [.cSharp, .dSharp, .fSharp, .gSharp, .aSharp]
-        return accidentals.contains(note.pitch)
-    }
-    
-    private struct PianoKeyButtonStyle: ButtonStyle {
-        var isAccidental: Bool
-        
-        private var verticalTranslation: CGFloat {
-            return isAccidental ? 1.0 : 1.005
-        }
-        
-        private var horizontalTranslation: CGFloat {
-            return isAccidental ? 0.9 : 0.94
-        }
-        
-        func makeBody(configuration: Self.Configuration) -> some View {
-            configuration.label
-                .scaleEffect(x: configuration.isPressed ? horizontalTranslation : 1.0, y: configuration.isPressed ? verticalTranslation : 1.0, anchor: .top)
-        }
-    }
-    
-    private struct PianoKeyShape: Shape {
-        var isAccidental: Bool
-        var radius: CGFloat
-        
-        func path(in rect: CGRect) -> Path {
-            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: radius, height: radius))
-            return Path(path.cgPath)
-        }
-    }
-    
-    var body: some View {
-        Button(action: {
-            print("Play \(self.note.pitch.rawValue) \(self.note.octave)")
-        }, label: {
-            PianoKeyShape(isAccidental: isAccidental, radius: radius)
-                .foregroundColor(self.color)
-                .overlay(
-                    PianoKeyShape(isAccidental: isAccidental, radius: radius)
-                        .stroke(Color.black, lineWidth: 0.5)
-                )
-        })
-        .frame(maxWidth: isAccidental ? accidentalKeySize.width : naturalKeySize.width, maxHeight: isAccidental ? accidentalKeySize.height : naturalKeySize.height)
-        .buttonStyle(PianoKeyButtonStyle(isAccidental: isAccidental))
-    }
-
-}
-
 struct PianoView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-//           PianoView(octave: 0, naturalKeyColor: .white, accidentalKeyColor: .black, backgroundColor: .gray)
-//              .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
-//              .previewDisplayName("iPhone 8")
-
             PianoView(naturalKeyColor: .yellow, accidentalKeyColor: .pink, backgroundColor: .purple, accentColor: .white)
               .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
               .previewDisplayName("iPhone 11 Pro Max")
-            
-//            PianoView(octave: 0, naturalKeyColor: .white, accidentalKeyColor: .black, backgroundColor: .gray)
-//                .previewDevice(PreviewDevice(rawValue: "iPad Pro (9.7-inch)"))
-//                .previewDisplayName("iPad Pro (9.7-inch)")
         }
     }
     
